5b7c25af67b7a2cb6d7d3827348f30dec72ec285
kpburgula <krishnapradeepburgula@gmail,com>
2023-01-07 13:56:46 +0530

    changes

---
+++ utils/01_1_global_theme.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.lightGreen,
+          canvasColor: Colors.white,
+          platform: TargetPlatform.iOS,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+        debugShowCheckedModeBanner: false);
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Theme(
+          // Unique theme with ThemeData - Overwrite
+            data: ThemeData(
+              cardColor: Colors.deepOrange,
+            ),
+            child: Card(
+              child: Text('Unique ThemeData'),
+            ),
+          ),
+          Theme(
+            // copyWith Theme - Inherit (Extended)
+            data: Theme.of(context).copyWith(cardColor: Colors.deepOrange),
+            child: Card(
+              child: Text('copyWith Theme'),
+            ),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ utils/flutter_app_test/android/app/src/main/java/kpburgula/github/io/flutter_app_test/MainActivity.java
@@ -0,0 +1,6 @@
+package kpburgula.github.io.flutter_app_test;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
